ed3efaae01a559fddb15d202f0d089fb3c7a782a
Byron Wilson <bywil5@morgan.edu>
2020-03-09 18:39:29 -0400

    Login Button Doesn't work

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -16,6 +16,15 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     @IBAction func onLoginButton(_ sender: Any) {
+        
+        let myURl = "https://api.twitter.com/oauth/request_token"
+        
+        TwitterAPICaller.client?.login(url: myURl, success: {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }, failure: { (Error) in
+            print("Unable to Login")
+        })
+        
     }
     
 
