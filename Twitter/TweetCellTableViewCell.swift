3cce3d53a9decc4f5e5a04c640e8a6801b1c8493
Byron Wilson <bywil5@morgan.edu>
2020-03-09 20:25:40 -0400

    Added Tweet Content and Profile Image(With Bugs)

---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Byron M Wilson on 3/9/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBOutlet weak var UserNameLabel: UILabel!
+    
+    @IBOutlet weak var TweetContent: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
